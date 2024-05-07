6df9684a5e0f1d2671b58f52bf83b11b3890ae02
sir-grr <grr1206@gmail.com>
2024-05-07 22:43:52 +0100

    added comment

--- translator.java
+++ translator.java
@@ -2,6 +2,7 @@
 
 public class translator {
 
+    //this is a filler class
     public static String translateString(String wordForTranslation){
         //here translation would be done from a variety of languages to english
         return wordForTranslation;
