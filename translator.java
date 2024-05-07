81e86f0fb769535906b9e35cfb6a166195f32bed
sir-grr <grr1206@gmail.com>
2024-05-07 21:52:05 +0100

    Created translator for example

---
+++ translator.java
@@ -0,0 +1,10 @@
+
+
+public class translator {
+
+    public static String translateString(String wordForTranslation){
+        //here translation would be done from a variety of languages to english
+        return wordForTranslation;
+    }
+    
+}
