b7a2388fb1e6d3b36c29d2f6a7e4fc00389ed6de
sir-grr <grr1206@gmail.com>
2024-05-07 22:08:06 +0100

    wordCounter added base functionality

--- wordCounter.java
+++ wordCounter.java
@@ -1,20 +1,34 @@
+import java.util.HashMap;
 
 public class wordCounter {
+    private HashMap<String, Integer> wordHolder;
 
-    public wordCounter(){
-        
+    public wordCounter(){//create a new hashmap
+        wordHolder = new HashMap<String, Integer>();
     }
 
-    public void addWord(){
-        translator.translateString("example");
+    public wordCounter(String[] words){//create a new hashmap and add the passed in words to it
+        wordHolder = new HashMap<String, Integer>();
+        addWords(words);
     }
 
-    public void addWords(){
-
+    public void addWord(String word){//translate a given word to english and then add it to the word counter
+        String englishWord = translator.translateString("word");
+        if(wordHolder.containsKey(englishWord)){
+            wordHolder.replace(englishWord,wordHolder.get(englishWord) + 1);
+        } else{
+            wordHolder.put(englishWord, 0);
+        }
     }
 
-    public int count(){
-        return 0;
+    public void addWords(String[] words){//pass an array of words to addWord
+        for(String word : words){
+            addWord(word);
+        }
+    }
+
+    public int countOf(String word){//return the count of a given word
+        return wordHolder.get(word);
     }
     
 }
