5efb0d7fd1f919a6bb981ea9148f1c672fcd4fc0
sir-grr <grr1206@gmail.com>
2024-05-07 21:52:22 +0100

    Initial wordCounter commit

---
+++ wordCounter.java
@@ -0,0 +1,20 @@
+
+public class wordCounter {
+
+    public wordCounter(){
+        
+    }
+
+    public void addWord(){
+        translator.translateString("example");
+    }
+
+    public void addWords(){
+
+    }
+
+    public int count(){
+        return 0;
+    }
+    
+}
